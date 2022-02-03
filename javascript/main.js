518a881ce83df0db30447ef4fc20699c03c48587
krihha <asterikas@icloud.com>
2022-02-03 20:02:31 +0200

    03.02

---
+++ javascript/main.js
@@ -0,0 +1,82 @@
+//  komment 
+/* mnogostro4nqj komment */
+
+let x = 10;
+
+//x = "hello"
+//console.debug('hello from jsfile');
+
+let firstname = "Kristina";
+
+
+console.log(x+firstname);
+console.log(x+x);
+
+// number 
+// string - text
+
+// boolean - true 1/false 0
+let isLocked = true;
+console.log(x+isLocked);
+
+// undefined 
+let u 
+//..
+u=10
+
+
+// NaN /not a number/ -> isNaN()
+
+console.log(x/'a');
+
+// null 
+let m = null
+
+
+//object  massivq
+let obj = [];
+let obj2 = {};
+
+console.log('x: ', typeof x );
+console.log('isLocked: ', typeof isLocked );
+console.log('m: ', typeof m );
+console.log('obj: ', typeof obj );
+console.log('obj2: ', typeof obj2);
+console.log('firstname: ', typeof firstname );
+
+
+
+if (x>30)
+{
+    console.log('x is grater than 50');
+} 
+else if ( x == 10) {
+    console.log('x is 10');
+}
+
+
+
+
+else {
+    console.log('x isn\'t grater than 50');
+}
+
+
+
+
+switch (x) {
+    case 10:
+        console.log('x=10');
+        
+
+        case 20:
+        console.log('x=20');
+       
+
+
+
+    default:
+        break;
+}
+
+console.log('');

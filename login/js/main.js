dc8a210d3077110f7238951ad054de4f05da0962
krihha <asterikas@icloud.com>
2022-02-24 17:32:48 +0200

    24.02

--- login/js/main.js
+++ login/js/main.js
@@ -2,6 +2,7 @@ let btnSubmit = document.getElementById('btnSubmit')
 let password = document.getElementById('password')
 let username = document.getElementById('username')
 let error = document.getElementById('error')
+let errorDescriptions = []
 
 // rules 
 // not empty
@@ -14,14 +15,40 @@ btnSubmit.addEventListener('click', function(){
     console.log('clicked');
     error.innerText = '';
     //1 username
+    errorDescriptions = []
     checkInputValue(username)
     checkInputValue(password)
+    if (errorDescriptions.length > 0)
+    {
+        try {
+          let content = document.getElementById ('modal-content')
+          content.innerText = errorDescriptions.join(' , \n ')
+          content.style.color = 'red'
+          MicroModal.init({
+            awaitCloseAnimation: true,// set to false, to remove close animation
+            onShow: function(modal) {
+              console.log("micromodal open");
+             
+    
+            },
+            onClose: function(modal) {
+              console.log("micromodal close");
+              content.style.color = 'black'
+            }
+          });
+            
+          } catch (e) {
+            console.log("micromodal error: ", e);
+          }   
+    }
+
 })
 
 function checkInputValue (input) 
 {
     let elementTitle = input.id
-    let errorDescriptions = []
+   
+    
 
     if (input.value.trim() == '')
     {
@@ -32,40 +59,39 @@ function checkInputValue (input)
         error.classList.remove ('d-none')
         error.style.display = 'block'
     } 
-    
     else if (
         input.value.trim().length <= 3 || 
         input.value.trim().length >=16)
     
-    {console.log('enter' + elementTitle + 'between 3-15 symbols');
-    error.innerText += 'enter '+ elementTitle + 'between 3-15 symbols \n'
+    {
+        console.log('enter' + elementTitle + 'between 3-15 symbols');
+        error.innerText += 'enter '+ elementTitle + 'between 3-15 symbols \n'
         errorDescriptions.push ('enter' + elementTitle + 'between 3-15 symbols')
 
 
-    error.style.setProperty ('display', 'block', 'important')
+        error.style.setProperty ('display', 'block', 'important')
 
     } else {
         error.classList.add ('d-none')
         error.removeAttribute ('style')
     }
-    if (errorDescriptions.length > 0)
-    {
-        try {
-      
-            MicroModal.init({
-              awaitCloseAnimation: true,// set to false, to remove close animation
-              onShow: function(modal) {
-                console.log("micromodal open");
-              },
-              onClose: function(modal) {
-                console.log("micromodal close");
-              }
-            });
-            
-          } catch (e) {
-            console.log("micromodal error: ", e);
-          }   
-    }
+    
 }
 
 
+document.addEventListener("DOMContentLoaded", function() {
+  
+    try {
+      
+     
+      MicroModal.init({
+        awaitCloseAnimation: true,// set to false, to remove close animation
+        
+      });
+
+      
+    } catch (e) {
+      console.log("micromodal error: ", e);
+    } 
+  });
+  
